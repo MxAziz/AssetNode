9948caadb50af95810077c0851273b79298472f3
MxAziz <hereabdulaziz@gmail.com>
2025-05-12 15:08:24 +0600

    complete my asset page

--- src/Pages/Employee/MyAsset.jsx
+++ src/Pages/Employee/MyAsset.jsx
@@ -1,6 +1,7 @@
 import useAuth from '../../hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
+import { toast } from 'react-toastify';
 
 const MyAsset = () => {
     const { user } = useAuth();
@@ -21,15 +22,28 @@ const MyAsset = () => {
       if (isLoading) return <p>Loading...</p>;
 
 
+  // Handle delete product
+  const handleCancelRequest = (id) => {
+    axiosPublic
+      .delete(`/products/${id}`)
+      .then((res) => {
+        if (res.data.deletedCount > 0) {
+          toast.success("Cancel Your Request!");
+          refetch();
+        }
+      })
+      .catch((error) => toast.error(error.message));
+  };
+
 
 
     console.log(requestAssets);
   return (
-    <div className="bg-gradient-to-b from-[#2F4749] to-[#F7C99B] text-white">
+    <div className="bg-gradient-to-b from-[#2F4749] to-[#e3b68a] text-white">
       <div className="overflow-x-auto py-14  w-11/12 mx-auto">
         <table className="table">
           {/* head */}
-          <thead>
+          <thead className="text-white">
             <tr>
               <th>#</th>
               <th>Asset Name</th>
@@ -40,6 +54,7 @@ const MyAsset = () => {
               <th>Action</th>
             </tr>
           </thead>
+                  <hr />
           <tbody>
             {/* dynamic row */}
             {requestAssets.map((asset, index) => (
@@ -48,7 +63,7 @@ const MyAsset = () => {
                 <td>{asset.assetName}</td>
                 <td>{asset.assetType}</td>
                 <td>{new Date(asset.requestDate).toLocaleDateString()}</td>
-                <td>Blue</td>
+                <td>N/A</td>
                 <td>{asset.requestStatus}</td>
                 <td>
                   {asset.requestStatus === "Pending" && (
