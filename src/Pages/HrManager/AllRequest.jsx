d5e794228b8cb48ae2108575e5bfbbe209e04e13
MxAziz <hereabdulaziz@gmail.com>
2025-05-13 09:36:57 +0600

    added approve and reject button functionality

--- src/Pages/HrManager/AllRequest.jsx
+++ src/Pages/HrManager/AllRequest.jsx
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 import useAuth from './../../hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
@@ -20,11 +21,26 @@ const AllRequest = () => {
     console.log(allRequestAsset);
 
     if (isLoading) return <p>Loading...</p>;
+
+    const handleStatusUpdate = async (id, status) => {
+    try {
+        const res = await axiosPublic.patch(`/updateRequestStatus/${id}`, {
+        status,
+        });
+        if (res.data.message === "Status updated successfully") {
+            toast.success("Status updated successfully");
+        refetch(); // data reload
+        }
+    } catch (error) {
+        console.error("Failed to update status", error);
+    }
+    };
+
     return (
       <div>
         {/* List Section */}
-        <div className="overflow-x-auto mt-4">
-          <table className="table w-full">
+        <div className="overflow-x-auto bg-gray-100 py-14">
+          <table className="table w-11/12 mx-auto">
             <thead>
               <tr className=" bg-[#2F4749] text-white ">
                 <th>Product Name</th>
@@ -43,18 +59,18 @@ const AllRequest = () => {
                   <td>{product.assetType}</td>
                   <td>{product.employeeName}</td>
                   <td>{product.employeeEmail}</td>
-                  <td>{new Date(product.requestStatus).toLocaleDateString()}</td>
+                  <td>{new Date(product.requestDate).toLocaleDateString()}</td>
                   <td>{product.requestStatus}</td>
                   <td className="flex justify-start items-center gap-2">
                     <button
                       className="rounded-md bg-[#2F4749] text-white hover:bg-[#F7C99B] hover:text-black px-4 py-[6px]  mr-2"
-                      onClick={() => setSelectedProduct(product)}
+                       onClick={() => handleStatusUpdate(product._id, "Approved")}
                     >
                       Approve
                     </button>
                     <button
                       className="rounded-md px-4 py-[6px] bg-[#F7C99B] text-black"
-                      onClick={() => handleDelete(product._id)}
+                    onClick={() => handleStatusUpdate(product._id, "Rejected")}
                     >
                       Reject
                     </button>
