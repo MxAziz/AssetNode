ff08321bd9f748d10e4a3e585c9d2bdf0391c284
MxAziz <hereabdulaziz@gmail.com>
2025-06-11 20:11:25 +0600

    formate authProvider page

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,35 +1,41 @@
 import { createContext, useEffect, useState } from "react";
-import auth from './../firebase/firebase.config';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import auth from "./../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import useAxiosPublic from "../hooks/useAxiosPublic";
-
 
 export const AuthContext = createContext(null);
 const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = ({children}) => {
-
-    const [user, setUser] = useState(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const axiosPublic = useAxiosPublic();
 
-    const signInWithGoogle = () => {
-      setLoading(true);
-      return signInWithPopup(auth, googleProvider);
-    };
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
-    const createUser = (email, password) => {
-      setLoading(true);
-      return createUserWithEmailAndPassword(auth, email, password);
-    };
-    const signInUser = (email, password) => {
-      setLoading(true);
-      return signInWithEmailAndPassword(auth, email, password);
-    };
-    const signOutUser = () => {
-      setLoading(true);
-      setUser(null);
-      return signOut(auth);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const signOutUser = () => {
+    setLoading(true);
+    setUser(null);
+    return signOut(auth);
   };
 
   const updateUserProfile = (name, photo) => {
@@ -73,22 +79,19 @@ const AuthProvider = ({children}) => {
     };
   }, []);
 
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signInUser,
+    signOutUser,
+    updateUserProfile,
+    signInWithGoogle,
+  };
 
-    const authInfo = {
-      user,
-      loading,
-      createUser,
-      signInUser,
-      signOutUser,
-      updateUserProfile,
-      signInWithGoogle,
-    };
-
-    return <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>
-
-
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
